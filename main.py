e3aa7270b87ce94f600c4c118666a0b31e04fcda
Krish Raj <krish.r2106@gmail.com>
2026-03-17 02:38:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+# #string
+# first_name = "Krish"
+# food = "Burger"
+# email = "krish.r2106@gmail.com"
+
+# #integers
+# age = 18
+# quantity = 5
+# no_of_students = 50
+
+# #float
+# price = 11
+# cgpa = 8.2
+# distance = 500
+
+# #boolean
+# is_online = True
+# is_student = False
+
+# name = "Krish Raj"
+# age = 18
+# gpa = 7.7
+# is_student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
+
+# name = input("Enter your Name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"Hello {name}")
+# print(f"You're {age} yrs old")
+
+friends = 3
+
+# friends += 1
+# friends = friends + 1          #argumented assignment operator
+# friends -= 1 
+# friends = friends - 1           #subtraction Operator
+# friends *= 5 
+# friends = friends * 5              #multiplication operator
+# friends /= 4 
+# friends = friends / 4             #division operator
+# friends **= 2 
+# friends = frineds ** 2            #exponential operator
+# friends //= 1 
+# friends = friends //2                #Floor Division
+print(friends)
